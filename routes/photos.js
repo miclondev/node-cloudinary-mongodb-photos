81d19721e4e0c6efc6c5bf0ffd4bc7dec74aca47
Mic Lon <miclon005@gmail.com>
2018-06-04 14:28:47 +0300

    styling homepage

--- routes/photos.js
+++ routes/photos.js
@@ -2,25 +2,29 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const Photo = mongoose.model('photos')
+const { isLoggedIn } = require('../middleware')
 
 //get recent photos
-router.get('/p/:page', (req, res) => { 
+router.get('/p/:page', (req, res) => {
     const page = parseInt(req.params.page)
-    let limit = 24
-    let skip
-    page ===  1 ? skip = 0 : skip = page * limit
+    let limit = 12
+    const skip = (page * limit) - limit;
+    // page === 1 ? skip = 0 : skip = page * limit
     console.log(skip)
     Photo.find({}).skip(skip).limit(limit).sort({ created_on: -1 })
-    .then((found) => {
-        console.log(found)
-        res.render('photos/index')
-    })
+        .exec((err, found) => {
+            if (err) {
+                console.log(err)
+                return res.send(err)
+            }
+            console.log(found)
+            res.render('photos/index')
+        })
 })
 
-router.post('/', (req, res) => {
+router.post('/', isLoggedIn, (req, res) => {
     const { title, shortDesc } = req.body;
     const newPhoto = { title, shortDesc, user: req.user._id }
-
     Photo.create(newPhoto, (err, created) => {
         if (err) {
             console.log(err)
